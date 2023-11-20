b34a267432ef01f9149b2d45f7fd15d5dc301f9b
BedratyiDV <bedrik2023@gmail.com>
2023-11-20 15:56:18 +0100

    First Commit
    20/11/2023

---
+++ MovieLibrary/MovieLibrary.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MovieLibrary
+{
+    public class MovieLibrary : IEnumerable<KeyValuePair<int, string>>
+    {
+        //public static readonly TimeSpan StartTime = new TimeSpan(23, 0, 0);
+        //public static readonly TimeSpan EndTime = new TimeSpan(7, 0, 0);
+        //public static readonly DateTime CurrentTime = DateTime.Now;
+
+        private Dictionary<int, string> _ordinaryMovies = new Dictionary<int, string>
+        {
+        { 0001, "ordinaryMovie_1" },
+        { 0002, "ordinaryMovie_2" },
+        { 0003, "ordinaryMovie_3" },
+        { 0004, "ordinaryMovie_4" }
+        };
+
+        private Dictionary<int, string> _onlyAdultMovies = new Dictionary<int, string>
+        {
+        { 0005, "onlyAdultMovie_1" },
+        { 0006, "onlyAdultMovie_2" },
+        { 0007, "onlyAdultMovie_3" },
+        { 0008, "onlyAdultMovie_4" }
+        };
+
+        public Dictionary<int, string> OrdinaryMovies
+        {
+            get => _ordinaryMovies;
+
+        }
+
+        public Dictionary<int, string> OnlyAdultMovies
+        {
+            get => _onlyAdultMovies;
+
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public IEnumerator<KeyValuePair<int, string>> GetEnumerator()
+        {
+            TimeSpan StartTime = new TimeSpan(23, 0, 0);
+            TimeSpan EndTime = new TimeSpan(7, 0, 0);
+            DateTime CurrentTime = DateTime.Now;
+
+            if (IsTimeInRange(CurrentTime, StartTime, EndTime))
+            {
+                return _ordinaryMovies.Concat(_onlyAdultMovies).GetEnumerator();
+            }
+            else
+            {
+                return _ordinaryMovies.GetEnumerator();
+            }
+        }
+        public IEnumerator<KeyValuePair<int, string>> GetEnumerator1()
+        {
+            return _ordinaryMovies.GetEnumerator();
+        }
+        public IEnumerator<KeyValuePair<int, string>> GetEnumerator2()
+        {
+            return _onlyAdultMovies.GetEnumerator();
+        }
+        public string GetMovie(int movieNum)
+        {
+
+            if (_ordinaryMovies.TryGetValue(movieNum, out string? value))
+            {
+                return value;
+            }
+            else
+
+            {
+                Console.WriteLine($"The film with unique key {movieNum} is not found in ordinary list ");
+
+                TimeSpan StartTime = new TimeSpan(23, 0, 0);
+                TimeSpan EndTime = new TimeSpan(7, 0, 0);
+                DateTime CurrentTime = DateTime.Now;
+
+                if (IsTimeInRange(CurrentTime, StartTime, EndTime))
+
+                {
+                    if (_onlyAdultMovies.TryGetValue(movieNum, out string? value1))
+
+                    {
+                        return value1;
+                    }
+                    else
+
+                    {
+                        return $"The film with unique key {movieNum} is not found in adult films list as well ";
+                    }
+                }
+                else
+
+                {
+                    return "It is not the time for adult movie";
+                }
+            }
+
+        }
+
+        static bool IsTimeInRange(DateTime currentTime, TimeSpan startTime, TimeSpan endTime)
+    {
+        TimeSpan currentTimeOfDay = currentTime.TimeOfDay;
+
+        return ((currentTimeOfDay >= startTime) && (currentTimeOfDay <= endTime));
+    }
+        
+            }
+}
+
+
---
+++ MovieLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MovieLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieLibrary allMovies = new MovieLibrary();
+            
+            foreach (KeyValuePair<int, string> movie in allMovies) 
+            
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", movie.Key, movie.Value);
+            }
+
+            Console.WriteLine("Key = 0002, Value = {0}", allMovies.GetMovie(0002));
+            Console.WriteLine("Key = 0007, Value = {0}", allMovies.GetMovie(0007));
+            Console.WriteLine("Key = 0008, Value = {0}", allMovies.GetMovie(0008));
+            Console.WriteLine("Key = 0009, Value = {0}", allMovies.GetMovie(0009));
+
+            Console.WriteLine(allMovies.OrdinaryMovies[0003]);
+            Console.WriteLine(allMovies.OnlyAdultMovies[0005]);
+
+            
+        }
+    }
+}
